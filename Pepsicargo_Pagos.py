c43de29d1edc35631a01ed18daa8338782bbb84b
Miguel_Data <48416245+MiguelData2030@users.noreply.github.com>
2024-12-15 21:25:19 -0500

    Update Pepsicargo_Pagos.py
    Actualizar ip publica

--- Pepsicargo_Pagos.py
+++ Pepsicargo_Pagos.py
@@ -14,7 +14,7 @@ if "proveedor" not in st.session_state:
 # Conexión a la base de datos
 usuario = 'postgres'
 contraseña = 'Luna2030.'
-host = 'localhost'
+host = '181.61.206.48'
 puerto = '5433'
 db = 'Proyecto_Pagos'
 
